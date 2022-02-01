2ea47812ee07e95d696750d2239e061533a07031
Sini Pajunen <sini.valimaa1@gmail.com>
2022-02-01 09:50:42 +0200

    Add MongoDB 3.12

---
+++ osa3/puhelinluettelo-backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+`mongodb+srv://fullstack:${password}@cluster0.7uonz.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number
+})
+
+if (process.argv.length === 3) {
+    Person
+    .find({})
+    .then(result => {
+        console.log('phonebook:')    
+    result.forEach(person => {
+      console.log(person.name, ' ', person.number)
+    })
+    mongoose.connection.close()
+    })  
+}
+
+if (process.argv.length > 3) {
+    person
+    .save()
+    .then(result => {
+        console.log('Added ',{name},' number ',{number},' to phonebook')
+    mongoose.connection.close()
+    })    
+}
