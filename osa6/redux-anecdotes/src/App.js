9e317d119a5176852e3678eca565df2b4491ba79
Sini Pajunen <sini.valimaa1@gmail.com>
2022-03-03 10:43:11 +0200

    Add osa6 exercise 6.8

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,14 @@
+import Anecdotes from './components/Anecdotes'
+import NewAnecdote from './components/NewAnecdote'
+
+const App = () => {
+
+  return (
+    <div>
+      <NewAnecdote/>
+      <Anecdotes/>
+    </div>
+  )
+}
+
+export default App
---
+++ osa6/redux-anecdotes/src/components/Anecdotes.js
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { voteOf } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({anecdote, handleClick}) => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  return(
+    <div style={style}>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const Anecdotes = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return(
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <Anecdote
+        key={anecdote.id}
+        anecdote={anecdote}
+        handleClick={() => 
+          dispatch(voteOf(anecdote.id))}
+        />)}
+    </div>
+  )
+}
+
+export default Anecdotes
